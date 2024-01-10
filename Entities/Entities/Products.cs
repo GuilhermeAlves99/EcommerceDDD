4f34b49fc5b032036d3db3f8f80ed059799b3b3c
Guilherme Alves <gui.a.v@hotmail.com>
2024-01-10 05:13:34 -0300

    Criação de Products para dar inicio a base de dados com informações básicas sobre  o produto

---
+++ Entities/Entities/Products.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    [Table("Product")]
+    public class Products:Notifies
+    {
+        [Column("PRD_ID")]
+        [Display(Name = "Code")]
+        public int Id { get; set; }
+
+        [Column("PRD_NAME")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Column("PRD_VALUE")]
+        [Display(Name = "Value")]
+        public decimal Value { get; set; }
+
+        [Column("PRD_STATUS")]
+        [Display(Name = "Status")]
+        public bool Status { get; set; }
+    }
+}
