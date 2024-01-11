3bb6a1fc7c4a81696288d2361ff31e1ebe7416f8
Guilherme Alves <gui.a.v@hotmail.com>
2024-01-11 01:34:39 -0300

    Criação dos CRUD's do produto e implementação da regra de  negócio

---
+++ Domain/Interfaces/InterfaceServices/IServiceProduct.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.InterfaceServices
+{
+    /// <summary>
+    /// Colocaremos aqui as regras de negócio para a interação dos CRUD'S
+    /// com os produtos na base de dados
+    /// </summary>
+    public interface IServiceProduct
+    {
+        Task AddProduct(Products product);
+
+        Task UpdateProduct(Products product);
+    }
+}
---
+++ Domain/Services/ServiceProduct.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Interfaces.InterfaceProducts;
+using Domain.Interfaces.InterfaceServices;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class ServiceProduct : IServiceProduct
+    {
+        private readonly IProduct _IProduct;
+        public ServiceProduct(IProduct IProduct) 
+        {
+            _IProduct = IProduct;
+        }
+
+        public async Task AddProduct(Products product)
+        {
+            var validateName = product.ValidatePropartiesString(product.Name, "Name");
+
+            var validateValue = product.ValidatePropertiesDecimal(product.Value, "Value");
+
+            if (validateName && validateValue) 
+            {
+                product.Status = true;
+                await _IProduct.Add(product);
+            }
+        }
+
+        public async Task UpdateProduct(Products product)
+        {
+            var validateName = product.ValidatePropartiesString(product.Name, "Name");
+
+            var validateValue = product.ValidatePropertiesDecimal(product.Value, "Value");
+
+            if (validateName && validateValue)
+            {
+                await _IProduct.Update(product);
+            }
+        }
+    }
+}
